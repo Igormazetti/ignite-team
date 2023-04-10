ca6dcbb7d3fc9cbbf9f11f605ace128ab38fc819
Igor <iigormazetti@hotmail.com>
2023-04-10 09:28:22 -0300

    Feat: added roboto font and loader component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
+import { ActivityIndicator, View, StyleSheet } from "react-native";
 import theme from "@theme/index";
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {!fontsLoaded ? (
+        <Groups />
+      ) : (
+        <View style={styles.loader}>
+          <ActivityIndicator />
+        </View>
+      )}
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
