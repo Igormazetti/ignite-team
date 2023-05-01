d47cbd9bfcb63ff300c42a0e554b6187c834c3f5
Igor Mazetti de Azevedo <iigormazetti@hotmail.com>
2023-05-01 10:48:50 -0300

    feat: added phosphor icons lib

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
+import { Header } from "@components/Header";
 import { Container, Title } from "./styles";
 import React from "react";
 
 export function Groups() {
   return (
     <Container>
-      <Title>index asd a da wdasd a</Title>
+      <Header showBackButton />
     </Container>
   );
 }
