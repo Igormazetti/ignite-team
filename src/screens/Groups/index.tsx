5e497cf78737a6aeb306c45de4f07b246e032436
Igor Mazetti de Azevedo <iigormazetti@hotmail.com>
2023-05-01 11:00:41 -0300

    Feat: created Highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@components/Header";
 import { Container, Title } from "./styles";
 import React from "react";
+import { Highlight } from "@components/Highlight";
 
 export function Groups() {
   return (
     <Container>
       <Header showBackButton />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
     </Container>
   );
 }
