cc62d5c5136e4686dedf45381139de42aa9ae245
Igor Mazetti de Azevedo <iigormazetti@hotmail.com>
2023-05-02 19:52:50 -0300

    Feat: created GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,15 @@
 import { Header } from "@components/Header";
-import { Container, Title } from "./styles";
+import { Container } from "./styles";
 import React from "react";
 import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header showBackButton />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      <GroupCard title="Galera do Ignite" onPress={() => console.log("alou")} />
     </Container>
   );
 }
