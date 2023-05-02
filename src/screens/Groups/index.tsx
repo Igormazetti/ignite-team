edcbd552cf0948460b68d72c4a0691c188b8476c
Igor Mazetti de Azevedo <iigormazetti@hotmail.com>
2023-05-02 20:00:41 -0300

    Feat: rendered GroupCard itens in a FlatList

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Header } from "@components/Header";
-import { Container } from "./styles";
-import React from "react";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera da RocketSeat",
+    "Amigos",
+    "Churrascada",
+  ]);
+
   return (
     <Container>
       <Header showBackButton />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" onPress={() => console.log("alou")} />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+            onPress={() => console.log("clicou no item da lista")}
+          />
+        )}
+      />
     </Container>
   );
 }
