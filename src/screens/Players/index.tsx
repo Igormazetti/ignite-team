b670834c4c240c051f339fe428eac7889105a3f2
Igor Mazetti de Azevedo <iigormazetti@hotmail.com>
2023-05-29 15:03:31 -0300

    feat: created flat list to list the teams

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,16 @@
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -18,7 +23,23 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
-      <Filter title="Time A" />
+
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal={true}
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
