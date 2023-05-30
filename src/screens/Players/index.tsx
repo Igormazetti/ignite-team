d60e57a1b50f3467a86b1f2aad5c0f088eef1a94
Igor Mazetti de Azevedo <iigormazetti@hotmail.com>
2023-05-30 08:32:03 -0300

    feat: created navigation config with stack navigation

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,6 +7,8 @@ import { Filter } from "@components/Filter";
 import { PlayerCard } from "@components/PlayerCard";
 import { FlatList } from "react-native";
 import { useState } from "react";
+import { ListEmpty } from "@components/ListEmpty";
+import Button from "@components/Button";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
@@ -48,7 +50,16 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas neste time!" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 70 },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
